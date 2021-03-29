b2d3837a107abb8ed075fd9e07ecded5e4422a39
clementia1 <grininma0@gmail.com>
2021-03-29 20:11:03 +0300

    *

---
+++ Module_3_Task_3/AsyncTask.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace Module_3_Task_3
+{
+    public class AsyncTask
+    {
+        private const string _filepath = "textfile.txt";
+
+        public async Task<string> ReadFileAsync(string filepath)
+        {
+            var fileContent = await File.ReadAllTextAsync(filepath);
+            return fileContent;
+        }
+
+        public async Task<string> GetWorldAsync()
+        {
+            return await Task.Run(() => "World");
+        }
+
+        public async Task<string> GetFullText()
+        {
+            var tasks = new List<Task<string>>();
+            tasks.Add(ReadFileAsync(_filepath));
+            tasks.Add(GetWorldAsync());
+
+            var completedTask = await Task.WhenAll(tasks);
+            return string.Join(", ", completedTask);
+        }
+    }
+}
---
+++ Module_3_Task_3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Module_3_Task_3
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var asyncTask = new AsyncTask();
+            var text = asyncTask.GetFullText().GetAwaiter().GetResult();
+            Console.WriteLine(text);
+        }
+    }
+}
